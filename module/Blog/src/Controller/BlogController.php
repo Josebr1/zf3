393708f6feebab34b38bde039c2e6a6eba95d49f
José Antônio <jose.silva.br@outlook.com>
2018-04-22 02:39:38 -0300

    Listando dados na view

--- module/Blog/src/Controller/BlogController.php
+++ module/Blog/src/Controller/BlogController.php
@@ -15,6 +15,18 @@ class BlogController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+
+        $posts = [
+            ['title' => 'Post 1', 'content' => 'Content 1'],
+            ['title' => 'Post 2', 'content' => 'Content 2'],
+            ['title' => 'Post 3', 'content' => 'Content 3'],
+            ['title' => 'Post 4', 'content' => 'Content 4'],
+            ['title' => 'Post 5', 'content' => 'Content 5'],
+            ['title' => 'Post 6', 'content' => 'Content 6'],
+        ];
+
+        return new ViewModel([
+            'posts' => $posts
+        ]);
     }
 }
