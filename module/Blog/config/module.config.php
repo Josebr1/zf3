1bc796beb4273698c90f97cba2e2013b2a2ead47
José Antônio <jose.silva.br@outlook.com>
2018-04-22 01:44:16 -0300

    riando nosso primeiro módulo

---
+++ module/Blog/config/module.config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silva
+ * Date: 22/04/18
+ * Time: 01:23
+ */
+
+namespace Blog;
+
+return [
+
+    'controllers' => [
+        'factories' => [
+
+        ]
+    ],
+    'router' => [
+
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'blog' => __DIR__."/../view"
+        ]
+    ]
+
+];
---
+++ module/Blog/src/Module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silva
+ * Date: 22/04/18
+ * Time: 01:22
+ */
+
+namespace Blog;
+
+class Module
+{
+
+    public function getConfig()
+    {
+        return include __DIR__ ."/../config/module.config.php";
+    }
+
+}
